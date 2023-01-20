de129608612b5d57187dfdab988971488b7f0d3c
Gor <7495500@gmail.com>
2023-01-20 15:41:45 +0300

    Generate solidus

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,19 @@ Bundler.require(*Rails.groups)
 
 module SolidusStart
   class Application < Rails::Application
+    if defined?(FactoryBotRails)
+      initializer after: "factory_bot.set_factory_paths" do
+        require 'spree/testing_support'
+        FactoryBot.definition_file_paths = [
+          *Spree::TestingSupport::FactoryBot.definition_file_paths,
+          Rails.root.join('spec/fixtures/factories'),
+        ]
+      end
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.active_storage.variant_processor = :mini_magick
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
